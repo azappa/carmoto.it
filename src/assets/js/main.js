62bff7c3059d8b263f20c227901299c9c21563de
Alessio Zappa <alessioedoardozappa@gmail.com>
2023-12-14 17:15:24 +0100

    2023

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -30,57 +30,6 @@
   window.addEventListener('scroll', onScroll);
   window.addEventListener('resize', onScroll);
 
-
-  //  -- loading latest items from autoscout24 --
-  reqwest({
-    url: 'https://zeit.carmoto.it/api/offers',
-    type: 'json',
-    method: 'get',
-    contentType: 'application/json',
-    crossOrigin: true,
-    error: function (err) {
-    },
-    success: function (resp) {
-      /*
-        successful response is:
-        [{
-          "link": "https://www.autoscout24.it/annunci/smart-fortwo-700-coupe-passion-45-kw-benzina-nero-78b21d5f-fcf0-4af0-9d03-92c9a39f62ff",
-          "name": "smart forTwo",
-          "version": "700 coupé passion (45 kW)",
-          "preview": "https://prod.pictures.autoscout24.net/listing-images/78b21d5f-fcf0-4af0-9d03-92c9a39f62ff_7df23a4f-c3e3-4471-8ca3-6d7175c41df0.jpg/420x315.jpg",
-          "price": "&#x20AC; 2.500,-<as24-footnote-item>Prezzo finale offerto al pubblico, comprensivo di IVA, non vincolato all&#x2019;acquisto di un finanziamento, a permuta o rottamazione. Passaggio di propriet&#xE0; e IPT esclusi.</as24-footnote-item>",
-          "data": "<ul data-item-name=\"vehicle-details\"><li>94.000 km</li><li>03/2005</li><li>45 kW (61 CV)</li><li>Usato</li><li>2 proprietari</li><li data-type=\"transmission-type\">Semiautomatico</li><li>Benzina</li><li>4,7 l/100 km (comb.)<as24-footnote-item>Ulteriori informazioni sul consumo dichiarato di carburante e sulle emissioni specifiche dichiarate di CO2 delle autovetture nuove sono reperibili nella guida sul consumo di carburante e sulle emissioni specifiche delle autovetture nuove, distribuita gratuitamente presso tutti i punti vendita e scaricabile dal sito: www.sviluppoeconomico.gov.it.</as24-footnote-item></li><li>113 g CO2/km (comb.)<as24-footnote-item>Ulteriori informazioni sul consumo dichiarato di carburante e sulle emissioni specifiche dichiarate di CO2 delle autovetture nuove sono reperibili nella guida sul consumo di carburante e sulle emissioni specifiche delle autovetture nuove, distribuita gratuitamente presso tutti i punti vendita e scaricabile dal sito: www.sviluppoeconomico.gov.it.</as24-footnote-item></li></ul>"
-        }]
-      */
-      if (resp && resp.length > 0) {
-        var latestContainer = document.getElementById('latest-ones');
-        resp.forEach(function (el, i) {
-          var thisEl = document.createElement('div');
-
-          thisEl.classList.add('pure-u-1-2');
-          thisEl.classList.add('pure-u-sm-1-3');
-          thisEl.classList.add('pure-u-md-1-5');
-          thisEl.classList.add('single-offer');
-
-          var elMarca = el.name !== '' ? '<h3>' + el.name + '</h3>' : '';
-          var elNome = el.version !== '' ? '<h5>' + el.version + '</h5>' : '';
-          var elImage = el.preview !== '' ? '<img src="' + el.preview + '" class="pure-img" />' : '<u>foto non disponibile</u>';
-          var elUrl = el.link !== '' ? '<p><a href="' + el.link + '" target="_blank">Link</a></p>' : '<p><u>link non disponibile</u></p>';
-
-          thisEl.innerHTML = '<div class="padding">' + elMarca + elNome + elImage + elUrl + '</div>';
-
-          latestContainer.appendChild(thisEl);
-
-          if (i === (resp.length - 1)) {
-            document.getElementById('latest').style.display = 'block';
-          }
-
-        });
-      }
-    }
-  });
-
-
   //  -- googlemaps --
   google.maps.event.addDomListener(window, 'load', init);
 
@@ -107,7 +56,7 @@
     window.google.maps.event.addDomListener(window, 'resize', function() {
       map.setCenter(new google.maps.LatLng(45.67948, 9.208471));
     });
-  }
+  }  
 
 })();
 
